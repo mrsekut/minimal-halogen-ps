786c7a6cef92b99fba63e2b09e15389a3e8593a6
mrsekut <k.cloudspider@gmail.com>
2020-05-31 02:14:40 +0900

    [Add] like Redux

--- src/Main.purs
+++ src/Main.purs
@@ -2,47 +2,60 @@ module Main where
 
 import Prelude
 
+import Control.Monad.State (get, put)
 import Data.Const (Const)
+import Data.Maybe (Maybe(..))
+
 import Effect (Effect)
 import Effect.Aff (Aff)
+
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
 
--- | Main
 
-main :: Effect Unit
-main = runHalogenAff do
-  body <- awaitBody
-  runUI (staticComponent staticHtml) unit body
+-- | Store
 
-
-
--- | Component
-
-type StaticHTML = H.ComponentHTML Unit () Aff
-staticComponent :: StaticHTML
-                -> H.Component HH.HTML (Const Void) Unit Void Aff
-staticComponent renderHtml =
+store :: H.Component HH.HTML (Const Void) Unit Void Aff
+store =
   H.mkComponent
-    { initialState: const unit
-    , render: \_ -> renderHtml
-    , eval: H.mkEval H.defaultEval
+    { initialState: const false
+    , render: toggleButton
+    , eval: H.mkEval $ H.defaultEval { handleAction = reducer }
     }
 
 
 
--- | Html
+-- | Modules
 
-staticHtml :: StaticHTML
-staticHtml =
-  HH.div_
-    [ HH.div_
-      [ HH.span_
-        [ HH.text "This is text in a span!" ]
-      ]
-    , HH.button_
-      [ HH.text "You can click me, but I don't do anything." ]
-    ]
+data Action = Toggle
+type State = Boolean
+
+reducer :: Action -> H.HalogenM State Action () Void Aff Unit
+reducer = case _ of
+  Toggle -> do
+    old <- get
+    put $ not old
+
+
+
+-- | Components
+
+toggleButton :: State -> H.ComponentHTML Action () Aff
+toggleButton isOn =
+    HH.button
+      [ HE.onClick \_ -> Just Toggle ]
+      [ HH.text $ "The button is " <> if isOn then "ON" else "OFF" ]
+
+
+
+-- | main
+
+main :: Effect Unit
+main =
+  runHalogenAff do
+    body <- awaitBody
+    runUI store unit body
